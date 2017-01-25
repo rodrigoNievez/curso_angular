3e77684925b1ddb715bafab1de1d0c17c722f385
Rodrigo Nievez <rodrigo.nievez@gmail.com>
2017-01-25 00:03:33 -0600

    Filtros ng-repeat

---
+++ Ejercicio_06_Repeticiones/js/app.js
@@ -0,0 +1,18 @@
+(function(){
+
+var app = angular.module('universidadApp',[ ]);
+
+app.controller('listadoCtrl', ['$scope', function($scope){
+	$scope.listado=["Fernando Herrera", "Melissa Flores","Juancarlos Pineda", "Maria Perez"];
+	
+		
+
+
+
+}]);
+
+
+
+
+
+})();
---
+++ Ejercicio_07_Repeticiones2/js/app.js
@@ -0,0 +1,38 @@
+(function(){
+
+var app = angular.module('universidadApp',[ ]);
+
+app.controller('listadoCtrl', ['$scope', function($scope){
+	$scope.listado=["Fernando Herrera", "Melissa Flores","Juan Carlos Pineda", "Maria Perez"];
+	$scope.listadoProfesores = {
+		profesores:[
+			{
+				nombre: "Fernando Herrera",
+				edad:29,
+				clase:"PEE"
+			},
+			{
+				nombre: "Melissa Flores",
+				edad:24,
+				clase:"ICE"
+			},
+			{
+				nombre:"Juan Carlos",
+				edad:42,
+				clase:"M110"
+			}
+		]
+		
+	};
+	
+		
+
+
+
+}]);
+
+
+
+
+
+})();
